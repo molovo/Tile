2bc139187ce0dfa5038d57be0025f19db2aba813
Jack Emerson <jackemerson@outlook.com>
2013-02-10 18:35:09 +0000

    Added styles and nav for admin

---
+++ tile/admin/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Admin - Edit your tile portfolio.</title>
+		<link rel="stylesheet" href="public/css/admin.css"/>
+	</head>
+	<body>
+		<h1>Edit your tile portfolio</h1>
+		<?php require_once 'partials/nav.php';?>
+	</body>
+</html>
---
+++ tile/admin/partials/nav.php
@@ -0,0 +1,5 @@
+<ul>
+	<li><a href="pages.php">Pages</a></li>
+	<li><a href="images.php">Images</a></li>
+	<li><a href="settings.php">Settings</a></li>
+</ul>
