0db6564c302ef96d36fd8bd049fd5d11559c6eff
Visual Idiot <iam@visualidiot.com>
2013-02-11 15:35:24 +0000

    changed the connection process

--- system/database.php
+++ system/database.php
@@ -1,13 +1,30 @@
 <?php
 
-//require connection array
-$config = require_once 'config.php';
-
 class Database {
 
-	public $connection;
+	private $_config;
+	public $connection = false;
 	
-	public function __construct($connection) {
-		$this->connection = new PDO ('mysql:host=' . $this->config['host'] .)
+	public function __construct($config) {
+		//  Save the config in its own object
+		$this->_config = $config;
+		
+		//  If there's no connection, try to start it
+		if($this->connection === false) {
+			try {
+				//  Build the query string
+				$connection = 'mysql:host=' . $this->_config['host'] . ';dbname=' . $this->_config['name'] . ';port=' . $this->_config['port'];
+		
+				//  Initiate the connection
+				$this->connection = new PDO($connection, $this->_config['user'], $this->_config['pass'], array(
+					PDO::ATTR_PERSISTENT => true
+				));
+		
+				//  Fix any character set issues
+				$this->connection->exec('set character set utf8');
+			} catch(PDOException $e) {
+				//  die(var_dump($e));
+			}
+		}
 	}
 }
