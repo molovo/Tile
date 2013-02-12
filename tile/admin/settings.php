1a850ba339e65f30ad350e5c377ea2d7f20d1c85
Jack Emerson <jackemerson@outlook.com>
2013-02-12 23:15:49 +0000

    corrected another typo

--- tile/admin/settings.php
+++ tile/admin/settings.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<title>Admin - Edit tile\'s settings.</title>
+		<title>Admin - Edit tile portfolio settings.</title>
 		<link rel="stylesheet" href="public/css/admin.css">
 	</head>
 	<body>
 		<?php include ('partials/nav.php'); ?>
-		<h1>Edit your tile portfolio\'s settings</h1>
+		<h1>Edit your tile portfolio settings</h1>
 	</body>
 </html>
