9bcd97ff3137f57c59c79cd152bd24c846e7fdb4
Jack Emerson <jackemerson@outlook.com>
2013-02-12 20:34:22 +0000

    added admin class

---
+++ system/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+/*  
+	*
+	Not too sure on this class
+	*
+*/
+class Admin {
+	
+	var $image;
+	var $connection;
+
+	//construct admin variables
+	public function __construct($newImage) {
+		$this->image = $newImage;
+		$this->connection = 
+	}
+
+	//function for storing image variable in database
+	public function storeImage() {
+		$this->connection->query('INSERT $image INTO table_name');
+	}
+
+	
+}
